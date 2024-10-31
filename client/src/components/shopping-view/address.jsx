18417fbc91e39e33c09fc8396f868f361836eb2c
Shef 8267 <shefiniqbal1998@gmail.com>
2024-11-01 00:16:09 +0530

    changes

---
+++ client/src/components/shopping-view/address.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import CommonForm from "../common/form";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { addressFormControls } from "@/config";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewAddress, fetchAllAddresses } from "@/store/shop/address-slice";
+
+const initialAddressFormData = {
+  address: "",
+  city: "",
+  phone: "",
+  pincode: "",
+  notes: "",
+};
+
+function Address() {
+  const [formData, setFormData] = useState(initialAddressFormData);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { addressList } = useSelector((state) => state.shopAddress);
+
+  function handleManageAddress(event) {
+    event.preventDefault();
+    dispatch(
+      addNewAddress({
+        ...formData,
+        userId: user?.id,
+      })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(fetchAllAddresses(user?.id));
+        setFormData(initialAddressFormData);
+      }
+    });
+  }
+
+  function isFormValid() {
+    return Object.keys(formData)
+      .map((key) => formData[key].trim() !== "")
+      .every((item) => item);
+  }
+  useEffect(() => {
+    dispatch(fetchAllAddresses(user?.id));
+  }, [dispatch]);
+
+  console.log(addressList, "addressList");
+  return (
+    <Card>
+      <div className="mb-5 p-3 grid grid-cols-1 sm:grid-cols-2  gap-2"></div>
+      <CardHeader>
+        <CardTitle>Add New Address </CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-3">
+        <CommonForm
+          formControls={addressFormControls}
+          formData={formData}
+          setFormData={setFormData}
+          buttonText={"Add"}
+          onSubmit={handleManageAddress}
+          isBtnDisabled={!isFormValid()}
+        />
+      </CardContent>
+    </Card>
+  );
+}
+export default Address;
---
+++ client/src/components/shopping-view/orders.jsx
@@ -0,0 +1,7 @@
+
+function  ShoppingOrders() {
+  return ( 
+    <div>Orders</div>
+  );
+}
+export default  ShoppingOrders;
